0be1600cb802e2f01ce5bf5de41a74f3989ac1a6
Michael Conner <michaelconner@hotmail.co.uk>
2019-01-23 13:18:09 +0000

    server file completed. router file get all completed

---
+++ client/src/app.js
@@ -0,0 +1,1 @@
+const Aspirations = require('./models/aspriations.js')
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+app.use(parser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('bucket_list');
+    const listItemsCollection = db.collection('listitems');
+    const listItemsRouter = createRouter(listItemsCollection);
+    app.use('/api/listitems', listItemsRouter);
+  })
+  .catch(console.err)
+
+app.listen(3000, function () {
+  console.log(`Lisatening on ${this.address().port}`);
+});
